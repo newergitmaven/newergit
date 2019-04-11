f80c55e7a6c84cb598e6b9e293588aa5aa091e9d
SONG <songxowe@hotmail.com>
2019-04-11 22:10:36 +0800

    amend by SONG

--- newer-git-maven/src/main/java/com/newer/App.java
+++ newer-git-maven/src/main/java/com/newer/App.java
@@ -8,5 +8,6 @@ public class App {
         System.out.println("Hello World!");
         System.out.println("Hello SONG");
         System.out.println("hello ");
+      System.out.println("Hi SONG - NEWER");
     }
 }
